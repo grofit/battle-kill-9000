ded23cea5c69924718481338fa4f0221047b99ea
LP <grofit1@yahoo.com>
2021-04-21 21:43:21 +0100

    Updated to .net 5
    Woop

--- src/BK9K/BK9K.Web/Program.cs
+++ src/BK9K/BK9K.Web/Program.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
-using BK9K.Web.Infrastructure.DI;
-using BK9K.Web.Modules;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using BK9K.Web.Infrastructure.DI;
+using BK9K.Web.Modules;
 
 namespace BK9K.Web
 {
@@ -17,7 +17,7 @@ namespace BK9K.Web
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddModule(new GameModule());
