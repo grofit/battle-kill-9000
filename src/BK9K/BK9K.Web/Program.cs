793ee2f0daa44af2331e57a73679c4aa077fc05f
LP <grofit1@yahoo.com>
2021-04-29 15:47:00 +0100

    Got DryIoC in, now testing EcsRx

--- src/BK9K/BK9K.Web/Program.cs
+++ src/BK9K/BK9K.Web/Program.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using BK9K.Web.Infrastructure.DI;
 using BK9K.Web.Modules;
+using DryIoc.Microsoft.DependencyInjection;
 
 namespace BK9K.Web
 {
@@ -17,6 +14,7 @@ namespace BK9K.Web
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.ConfigureContainer(new DryIocServiceProviderFactory());
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
